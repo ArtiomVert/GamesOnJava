2e1f8db9e806d92bf76c12ab5045e253781ed52a
ArtiomVert Acer <asayarov@gmail.com>
2023-12-23 01:15:00 +0300

    Task 2.2 and 2.3 completed. Task 2 fully completed.

--- src/ru/View.java
+++ src/ru/View.java
@@ -13,4 +13,22 @@ public class View{
 	public void print(Object s){
 		System.out.print(s);
 	}
+	public String ch(int n){
+		switch(n){
+			case 0:
+				return " ";
+			case 1:
+				return "X";
+			case 2:
+				return "O";
+		}
+		return " ";
+	}
+	public void printDesk(int[][] desk){
+		println(ch(desk[0][0]) + "|" + ch(desk[0][1]) + "|" + ch(desk[0][2]));
+		println("-+-+-");
+		println(ch(desk[1][0]) + "|" + ch(desk[1][1]) + "|" + ch(desk[1][2]));
+		println("-+-+-");
+		println(ch(desk[2][0]) + "|" + ch(desk[2][1]) + "|" + ch(desk[2][2]));
+	}
 }
--- src/ru/games/CresNolGame.java
+++ src/ru/games/CresNolGame.java
@@ -2,6 +2,7 @@ package ru.games;
 import ru.View;
 import ru.fields.CresNolField;
 import java.util.Scanner;
+import java.util.Random;
 public class CresNolGame{
 	View v = new View();
 	public void gameMenu(){
@@ -17,13 +18,13 @@ public class CresNolGame{
 			String comand = inp.nextLine();
 			switch(comand){
 				case "1":
-					//playWithBot(1);
+					playWithBot(1);
 					break;
 				case "2":
-					//playWithBot(2);
+					playWithBot(2);
 					break;
 				case "3":
-					//playWithHuman();
+					playWithHuman();
 					break;
 				case "4":
 					v.clean();
@@ -35,5 +36,100 @@ public class CresNolGame{
 					break;
 			}
 		}
+	}
+	private int[] inputTwoInt(){
+		try {
+        	Scanner in = new Scanner(System.in);
+        	String[] s = in.nextLine().split(" ");
+        	return new int[] {Integer.parseInt(s[0]), Integer.parseInt(s[1])};
+    	} catch (Exception e) {
+        	v.println("Пожалуйста, попробуйте снова.");
+        	return inputTwoInt();
+    	}
+	}
+
+	private void playWithBot(int who){
+		CresNolField field = new CresNolField();
+		boolean end = false;
+		for (int i = 0; i < 9; i++){
+			v.clean();
+			v.printDesk(field.desk);
+			if ((i+who-1)%2==1){
+				field.botHod(who);
+			}else{
+				v.println("Ходит игрок("+v.ch(who)+"). Введите поле, куда хотите сходить(строка, столбец). Например '3 3'");
+				int x = 0;
+				int y = 0;
+				boolean f = true;
+				do{
+					int[] place = inputTwoInt();
+					x = place[0]-1;
+					y = place[1]-1;
+					try{
+						if (field.desk[x][y]==0){
+							f = false;
+						}else{
+							v.println("Это место занято, попробуйте другое.");
+						}
+			    	}catch(Exception e){
+			    		v.println("Пожалуйста, попробуйте снова.");
+			    	}
+				}while(f);
+				field.desk[x][y] = who;
+			}
+			v.clean();
+			if (field.itisend()){
+				end = true;
+				if ((i+who-1)%2==1){
+					v.println("Бот выиграл");
+				}else{
+					v.println("Игрок выиграл");
+				}
+				
+				break;
+			}
+		}
+		if (end == false){
+			v.println("Победила дружба! :)");
+		}
+		v.printDesk(field.desk);
+	}
+
+	private void playWithHuman(){
+		CresNolField field = new CresNolField();
+		boolean end = false;
+		for (int i = 0; i < 9; i++){
+			v.clean();
+			v.printDesk(field.desk);
+			v.println("Ходит игрок " + (1 + i%2) + " (" + v.ch(1 + i%2) + "). Введите поле, куда хотите сходить(строка, столбец). Например '3 3'");
+			int x = 0;
+			int y = 0;
+			boolean f = true;
+			do{
+				int[] place = inputTwoInt();
+				x = place[0]-1;
+				y = place[1]-1;
+				try{
+					if (field.desk[x][y]==0){
+						f = false;
+					}else{
+						v.println("Это место занято, попробуйте другое.");
+					}
+			    }catch(Exception e){
+			    	v.println("Пожалуйста, попробуйте снова.");
+			    }
+			}while(f);
+			v.clean();
+			field.desk[x][y] = 1+i%2;
+			if (field.itisend()){
+				end = true;
+				v.println("Игрок " + (1+i%2) +" выиграл");
+				break;
+			}
+		}
+		if (end==false){
+			v.println("Победила дружба! :)");
+		}
+		v.printDesk(field.desk);
 	}
 }
